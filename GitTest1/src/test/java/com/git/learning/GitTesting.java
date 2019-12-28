06cbd7f834c8c6db293b374fb162526f0cf6031b
enuxka804 <enuxka804@gmail.com>
2019-12-27 23:24:30 -0500

    TLK-103 figuring out.

--- GitTest1/src/test/java/com/git/learning/GitTesting.java
+++ GitTest1/src/test/java/com/git/learning/GitTesting.java
@@ -3,7 +3,7 @@ package com.git.learning;
 public class GitTesting {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	System.out.println("Hello Elnur! Go fight");
 
 	}
 
